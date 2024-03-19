6e7484c5a191a2e0c2976c55198d1d7f20614f0f
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-03-19 07:03:29 -0300

    Criando a primeira rota da API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
+const users = require('./mocks/users');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end('<h1>Hello World!</h1>')
+    console.log(`Request Method: ${req.method} | Request Endpoint: ${req.url}`)
+
+    if(req.url === '/users' && req.method === 'GET'){
+        res.writeHead(200, {'Content-Type' : 'aplication/json'});
+        res.end(JSON.stringify(users));
+    } else {
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end(`Cannot ${req.method} ${req.url}`);  
+    }
+    
+    // res.writeHead(200, {'Content-Type' : 'text/html'});
+    // res.end('<h1>Hello World!</h1>')
 });
 
 server.listen(3000, () => console.log("Server started at http://localhost:3000"));
---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1, 
+        name: "Frederico"
+    }, 
+    {
+        id: 2, 
+        name: "Isadora"
+    }, 
+    {
+        id: 3, 
+        name: "Ana"
+    }, 
+    {
+        id: 4, 
+        name: "Be"
+    }
+]
