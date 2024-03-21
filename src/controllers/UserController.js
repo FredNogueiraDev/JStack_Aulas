5354f560f4896d070f00236202f654eb6416cdf3
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-03-21 07:03:14 -0300

    Capturando o body das requests

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,7 @@
 const users = require('../mocks/users');
 
 module.exports = {
-    listUsers(req, res){
+    listUsers(req, res) {
         const { order } = req.query;
 
         const sortedUsers = users.sort((a, b) => {
@@ -13,7 +13,7 @@ module.exports = {
         
         res.send(200, sortedUsers);
     },
-    getUserById(req, res){
+    getUserById(req, res) {
         const { id } = req.params;
 
         const user = users.find((user) => user.id === Number(id))
@@ -24,5 +24,26 @@ module.exports = {
 
         res.send(200, user);
     },    
+    createUser(req, res) {
+        let body = "";
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            body = JSON.parse(body);
+
+            const lastUserId = users[users.length - 1].id;
+            const newUser = {
+                id: lastUserId + 1,
+                name: body.name,
+            }
+
+            users.push(newUser);
+
+            res.send(200, users);
+        })
+    }
 };
 
